f15cbb6ef4226e7290502d02742ba87d8fdb1765
ivanperez-opti <ivan.perez@optimen.com.mx>
2025-12-11 10:24:44 -0600

    test: Data de amplify

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { Amplify } from 'aws-amplify';
-import outputs from './amplify_outputs.json';
+// import outputs from '../amplify_outputs.json'; // TEST EN AMBIENTE LOCAL
+// import { awsExports } from './aws-exports';
+// Amplify.configure(awsExports);
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs); // TEST EN AMBIENTE LOCAL
 
 // bootstrapApplication(App,{
 //   providers: [provideCharts(withDefaultRegisterables())],
